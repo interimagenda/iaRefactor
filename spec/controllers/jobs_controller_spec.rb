4fd5c53eafe887bc0080124a641103f4f14780df
Rogier Nitschelm <rogiernitschelm@live.nl>
2016-08-21 12:40:32 +0200

    add case for #new for jobs

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe JobsController, type: :controller do
   # index
 
   describe "GET #index" do
+
     it "returns http success" do
       get :index
       expect(response.status).to eq(200)
@@ -19,6 +20,28 @@ RSpec.describe JobsController, type: :controller do
       Job.create
       get :index
       expect(assigns(:jobs.length)).to_not eq(0)
+    end
+
+  end
+
+  # new
+
+  describe "GET #new" do
+
+    before do
+      get :new
+    end
+
+    it "gets new" do
+      assert_response :success
+    end
+
+    it "returns http success" do
+      expect(response.status).to eq(200)
+    end
+
+    it "renders the new template" do
+      expect(response).to render_template("new")
     end
 
   end
